23b14ee8bdbbe6a6d3444ce6e78d642d930bb124
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-27 09:19:26 +0600

    capcha

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home";
 import Menu from "../Pages/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
 
   export const router = createBrowserRouter([
     {
@@ -21,6 +22,10 @@ import Order from "../Pages/Order/Order/Order";
         {
           path: 'order/:category',
           element: <Order></Order>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
         }
         
       ]
